07f8ac1ef2187d58c8c95f5ada5b8be005d24d5a
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2025-02-19 13:49:35 +0200

    created model profile, add forms.py and profile_setup.html

--- network/models.py
+++ network/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class ProfileSetup(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to="profile_pics/", default="default.jpg")
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=100,blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True,)
+    
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
