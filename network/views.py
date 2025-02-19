35d4c1bfe66c9f24cf2e76f5a6d0ad38d71678ae
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2025-02-19 16:06:31 +0200

    modified profile_setup.html and profile_setup function

--- network/views.py
+++ network/views.py
@@ -63,8 +63,34 @@ def register(request):
         return render(request, "network/register.html")
 
 def profile_setup(request):
-    #fetch User data
+    #fetch User data and accesing profile
     current_user = request.user
-    return render("network/profile_setup.html", {
+    profile = ProfileSetup.objects(user=current_user)
+    if request.method == "POST":
+        #fetch input data
+        first_name = request.POST("first_name")
+        last_name = request.POST("last_name")
+        date_birth = request.POST("date_of_birth")
+        location = request.POST("location")
+        email = request.POST("email")
+        bio = request.POST("bio")
+        profile_picture = request.FILES.get("profile_picture")
+
+        #saving data in user model
+        current_user.first_name = first_name
+        current_user.last_name = last_name
+
+        if current_user.email is not email:
+            current_user.email = email
+        
+        #saving data in profile_setup model
+        profile.birth_date = date_birth
+        profile.location = location
+        profile.bio = bio
+        profile.proile_picture = profile_picture
+
+        return HttpResponseRedirect(reverse("index"))
+
+    return render(request,"network/profile_setup.html", {
         "user":current_user
     })
