7d992e669e3d0c128d4341c2fd5c05e5509b3152
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2025-02-20 15:37:49 +0200

    create post model

--- network/models.py
+++ network/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     pass
 
 class ProfileSetup(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     profile_picture = models.ImageField(upload_to="profile_pics/", default="default.jpg")
     bio = models.TextField(blank=True)
     location = models.CharField(max_length=100,blank=True)
@@ -15,3 +15,12 @@ class ProfileSetup(models.Model):
     
     def __str__(self):
         return f"{self.user.username}'s Profile"
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
+    content = models.TextField()
+    image = models.ImageField(upload_to="post_images/", blank=True, null=True)  # Optional field
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post by {self.user.username} - {self.created_at.strftime('%Y-%m-%d')}"
