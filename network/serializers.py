0d004a8a32b98dd13a5735d359c32fe2ac7fd532
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2025-02-20 16:19:45 +0200

    modified serializers.py added post serializer class

--- network/serializers.py
+++ network/serializers.py
@@ -1,4 +1,16 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post,User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ["password"]
 
 #api/serializer for post
+class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Post
+        fields = '__all__'
