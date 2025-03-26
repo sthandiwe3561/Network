69a28ede37577ce29f8f03d2d9cbc79556b7b883
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2025-03-26 15:21:31 +0200

    modified create_profile and styles.css

--- network/tests.py
+++ network/tests.py
@@ -1,6 +1,7 @@
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 from network.models import Profile
 import io
 from PIL import Image
@@ -8,7 +9,7 @@ from PIL import Image
 # Create your tests here.
 class ProfileModelTest(TestCase):
     def setUp(self):
-        """Create a sample profile for testing."""
+        """Create a sample profile model for testing."""
          # Create a user/ecause of the user foreign field in profile
         User = get_user_model()
         self.user = User.objects.create_user(username="sthandiwe", password="password123")
@@ -37,6 +38,42 @@ class ProfileModelTest(TestCase):
         self.assertEqual(self.profile.username, "sthandiwe")
         self.assertTrue(self.profile.image.name.startswith('uploads/test_image'))
         self.assertEqual(self.profile.bio, "I have this in life")
+
+#profilefunction test case
+class ProfileViewTest(TestCase):
+    def setUp(self):
+         #for making HTTP request to stimulate web user testing functions
+        self.client = Client()
+        User = get_user_model()
+        self.user = User.objects.create_user(username="sthandiwe", password="password123")
+        self.client.login(username="sthandiwe", password="password123")
+
+    def test_profile_get_request(self):
+        #show the profile form reverse to the profile form
+        response = self.client.get(reverse('profile'))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response, '<form')
+
+    def test_profile_post_request(self):
+        """Test if a profile is created/updated correctly."""
+        image_file = io.BytesIO()
+        image = Image.new("RGB", (100, 100), color=(255, 0, 0))
+        image.save(image_file, format="PNG")
+        image_file.name = "test_image.png"
+        image_file.seek(0)
+
+        response = self.client.post(reverse('profile'), {
+            "username": "new_username",
+            "description": "Updated bio",
+            "image": SimpleUploadedFile(image_file.name, image_file.read()),
+        })
+
+        self.assertEqual(response.status_code, 302)  # Should redirect to index
+        self.assertTrue(Profile.objects.filter(user=self.user, username="new_username").exists())
+            
+
+
+
 
         
 
