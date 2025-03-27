70e412ac7b5c1dc0f806bc1117859dbbe243ea0f
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2025-03-27 12:30:10 +0200

    created post model and modified profile model

--- network/models.py
+++ network/models.py
@@ -6,10 +6,21 @@ class User(AbstractUser):
     pass
 
 class Profile(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE, related_name="Profile_user")
     username = models.CharField(max_length=100)
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
     bio = models.TextField()
     
     def __str__(self):
         return f"{self.username} by {self.user.username}"
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
+    content = models.TextField()
+    image = models.ImageField(upload_to="posts/", blank=True, null=True)  # Optional field
+    created_at = models.DateTimeField(auto_now_add=True)
+    hide = models.BooleanField(default=False)
+    likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)  # Users who liked the post
+
+    def __str__(self):
+        return f"Post by {self.user.username} - {self.created_at.strftime('%Y-%m-%d')}"
